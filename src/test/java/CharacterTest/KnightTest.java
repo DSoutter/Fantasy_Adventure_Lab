fc07c19c395ba4af43c2d38aba01e46859f969ab
DSoutter <duncan.soutter@gmail.com>
2021-10-06 13:40:07 +0100

    knight has armour

--- src/test/java/CharacterTest/KnightTest.java
+++ src/test/java/CharacterTest/KnightTest.java
@@ -1,9 +1,22 @@
 package CharacterTest;
 
+import Character.Melee.Knight;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class KnightTest {
+
+    Knight knight;
+
+    @Before
+    public void before(){
+        knight = new Knight("Sofia", 300.00, 100.00);
+    }
+
+    @Test
+    public void knightHasArmour(){
+        assertEquals(100.00, knight.getArmour(),0.01);
+    }
 }
